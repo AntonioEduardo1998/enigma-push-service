d6afc287912fc73bdb1bee0dc2d84c4086f5a3b4
Antonio Eduardo <antonioeduardo1998@gmail.com>
2022-04-27 20:37:58 -0300

    feat: implemented swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { EnigmaModule } from './enigma.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(EnigmaModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Enigma API')
+    .setDescription('The Enigma API')
+    .setVersion('1.0')
+    .addTag('enigma')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
