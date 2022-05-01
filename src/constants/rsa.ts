c306a55cd64661c0428a95eba45b42b442e5d75b
unknown <maildocyrillo@gmail.com>
2022-05-01 19:13:21 -0300

    feat: Add twillo api

---
+++ src/constants/rsa.ts
@@ -0,0 +1,16 @@
+export const RSA_TOKEN =
+  '-----BEGIN RSA PRIVATE KEY-----\n' +
+  'MIICXAIBAAKBgH7Ei2qZWnbhc+B6cgMaRJf0jR5CWqKzyqCv5f+Xl03iPYdq82up\n' +
+  'GXAw+Z9RXPuAeoRdszuq7tC6mojFz8+yob7EdAiuF/w5Yd8OLlnp9kWszFKOs6dV\n' +
+  'mh7THaYtgPBtDRgjFHInHYMPdMHUY3Qwn2VrQADid4kW8sQ029w9wk9rAgMBAAEC\n' +
+  'gYBY98HM56HI60OzGNxpdwcLCnP795+6rqoYCjtGHXLwE0cRgisitQQIbCbU/Hpg\n' +
+  'fHP/7+75gmanET+hnArT/s9qnicCklAYOTdP2mvI32rTAaIXqLSoSK9P8t2nDcxe\n' +
+  'fbe8jaq1nYSvJ8byCgLkNyo/CYPAMc1dd+PPk0C3CedDsQJBANP/fKQgQhXI0Co2\n' +
+  'ma6aGr/q/xuGFgw6y1Jj/jZgZu4fh/4UWGtk8Xs2dW5gjyCrElAd58Adj+3MJiHf\n' +
+  '40w0lRMCQQCZFFlQ2D/YVuQrMhQ/MOWIne2EwMtykqUlmpJKHeFuBIr3UI2Lr66O\n' +
+  'yGBaKXynGlUfRRB1pX0lsWM1rKb7v59JAkEAyni4Egi9FRE1vxtmq9uCsMk/3Imz\n' +
+  'hPqiNY5KvKRVqhdzSrcju24tNABPJ5wRFqJMWzg0nsedLwBLaw7ph16xAQJAM8GL\n' +
+  'iSf0PiLytiASdGMF2DD9M1sz7iLir1oZh/GeXGIw0flsQgQ+Eoxw90r7mEvw58ek\n' +
+  'DwLK00msb2NWeduJ8QJBAJdp2pelCVdnEWCh8jUUwvwgzT6AUaszKeIrwyWSvOzq\n' +
+  '5wp4yftIG/OMJiSOg/+pLqrTzVQs+loQ0kSbgPp0Ss0=\n' +
+  '-----END RSA PRIVATE KEY-----';
--- src/services/enigma.service.ts
+++ src/services/enigma.service.ts
@@ -1,27 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { RSA_TOKEN } from 'src/constants/rsa';
 import { Historic } from 'src/interfaces/Historic';
 import { ReturnMessage } from 'src/interfaces/ReturnMessage';
 import { EnigmaRepository } from 'src/repositories/enigma.repository';
 
+const accountSid = 'AC63fff6ae4c45d080fb9fded8d110d11c';
+const authToken = 'c3555e3a612b0841cb1e43e4db1cc073';
+
 @Injectable()
 export class EnigmaService {
-
-  constructor(private enigmaRepository: EnigmaRepository) { }
+  constructor(private enigmaRepository: EnigmaRepository) {}
 
   public getKeysHistoric(): Historic[] {
     return this.enigmaRepository.getKeysHistoric();
   }
 
+  public sendSMS(key: string, phone: string) {
+    const client = require('twilio')(accountSid, authToken);
+    const NodeRSA = require('node-rsa');
+
+    const RSA = new NodeRSA(RSA_TOKEN);
+
+    const decrypted = RSA.decrypt(key, 'utf8');
+
+    client.messages
+      .create({
+        to: phone,
+        from: '+19897350226',
+        body: 'Novas diretrizes: ' + decrypted,
+      })
+      .then((message) => console.log(message.sid))
+      .done();
+
+    return decrypted;
+  }
+
   public sendDecryptKey(key: string): string {
     const phones = this.enigmaRepository.listNotificationPhones();
-    phones.forEach(phone => {
-      console.log(`Sending decrypt key (${key}) to ${phone}...`);
+
+    phones.forEach((phone) => {
+      const descrypted = this.sendSMS(key, phone);
+
       this.enigmaRepository.saveHistoric({
-        key,
+        key: descrypted,
         phone,
         date: new Date(),
-      })
+      });
     });
+
     return 'Keys sent!';
   }
 
